521a65b1f37a91aa4452085feca3650b7eeff941
Megha V Hegde <MeghaV.Hegde@ucb.com>
2023-08-08 23:00:00 +0530

    Add project files.

---
+++ FileComparer/Controllers/FileComparerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using FileComparer.Models;
+
+namespace FileComparer.Controllers
+{
+    public class FileComparerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CompareAndCopy(string oldFilePath, string newFilePath)
+        {
+            if (string.IsNullOrEmpty(oldFilePath) || string.IsNullOrEmpty(newFilePath))
+            {
+                ViewBag.ResultMessage = "Please enter file paths.";
+                return View("Index");
+            }
+
+            try
+            {
+                var model = new FileComparerModel();
+                model.CompareAndCopyFiles(oldFilePath, newFilePath);
+
+                ViewBag.ResultMessage = "Content copied successfully!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ResultMessage = "Error: " + ex.Message;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ FileComparer/Models/FileComparerModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileComparer.Models
+{
+    public class FileComparerModel
+    {
+        public void CompareAndCopyFiles(string oldFolderPath, string newFolderPath)
+        {
+            string[] oldFiles = Directory.GetFiles(oldFolderPath);
+            string[] newFiles = Directory.GetFiles(newFolderPath);
+
+            foreach (string oldFile in oldFiles)
+            {
+                string oldFileName = Path.GetFileName(oldFile);
+                string newFile = Array.Find(newFiles, file => Path.GetFileName(file).Equals(oldFileName, StringComparison.OrdinalIgnoreCase));
+
+                if (newFile != null)
+                {
+                    string oldFileContent = File.ReadAllText(oldFile);
+                    string newFileContent = File.ReadAllText(newFile);
+
+                    int dataIndex = newFileContent.IndexOf("Data", StringComparison.OrdinalIgnoreCase);
+                    if (dataIndex >= 0)
+                    {
+                        newFileContent = newFileContent.Substring(0, dataIndex + "Data".Length);
+                        newFileContent += Environment.NewLine + oldFileContent;
+                        File.WriteAllText(newFile, newFileContent);
+                    }
+                    else
+                    {
+                        throw new Exception($"The text 'Data' was not found in the new file: {newFile}");
+                    }
+                }
+            }
+        }
+    }
+}
